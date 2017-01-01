c8adcfe3a3a8c1f36df89c8b490df9a366403436
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-01 18:25:49 -0200

    Style refined

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,11 @@
         <div class="col-md-8 col-md-offset-2">
 
            <div class="panel panel-default">
-                <div class="panel-heading">Posts</div>
+                <div class="panel-heading">
+                    <h1>Dashboard - 
+                        <a href="#" class="btn btn-default" role="button">Novo Post</a>
+                    </h1>
+                </div>
                 
                 <div class="panel-body">
                     @forelse($posts as $post)
@@ -20,9 +24,12 @@
                                 <h4 class="media-heading">{{$post->title}}</h4>
                                 <p>{{$post->content}}</p>
                                 <p><small><b>Created by:</b> {{$post->user->name}}</small> <small><b>at</b> {{$post->created_at}}</small></p>
-                                <a class="btn btn-primary" href="{{url("post/$post->id/update")}}" role="button">Editar</a>
-                                <a class="btn btn-success" href="#" role="button">Publicar</a>
-                                <a class="btn btn-danger" href="#" role="button">Deletar</a>
+                                <a class="btn btn-warning" href="#" role="button">Visualizar</a>
+                                @can('who-see', $post)
+                                    <a class="btn btn-primary" href="{{url("post/$post->id/update")}}" role="button">Editar</a>
+                                    <a class="btn btn-success" href="#" role="button">Publicar</a>
+                                    <a class="btn btn-danger" href="#" role="button">Deletar</a>
+                                @endcan
                             </div>
                         </div>
                         <hr>
